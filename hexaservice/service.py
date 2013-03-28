bf7f15b519a305cb6ead3383659e9a1edb8f901b
hexascroller <phooky+hexascroller@gmail.com>
2013-03-28 19:29:35 -0400

    Reduced to a basic clock service

--- hexaservice/service.py
+++ hexaservice/service.py
@@ -2,19 +2,26 @@
 import led_panel
 from led_panel import panels
 import time
+import signal
+import sys
 
 class ServiceThread:
     pass
 
 if __name__=="__main__":
     led_panel.init()
-
     panels[0].setRelay(True)
-
-    for i in range(100):
+    def sigint_handler(signal,frame):
+	print("Caught ctrl-C; shutting down.")
+	panels[0].setRelay(False)
+        led_panel.shutdown()
+        sys.exit(0)
+    signal.signal(signal.SIGINT,sigint_handler)
+    while True:
+        msg = time.strftime("%H:%M:%S")
         for j in range(3):
-            panels[j].setMessage("Test:"+str(i),i,0)
-        time.sleep(0.333)
+            panels[j].setMessage(msg,0,0)
+        time.sleep(0.15)
 
     panels[0].setRelay(False)
 
