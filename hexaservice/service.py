14b46bfbe6b3906cdb5137842476f84197df4328
Guillaume RISCHARD <git@stereo.lu>
2023-04-26 14:03:07 -0400

    Sleep only in panel thread

--- hexaservice/service.py
+++ hexaservice/service.py
@@ -303,6 +303,9 @@ def panel_update():
             state.bitmap = new_bitmap
         # Sleep for a while
         time.sleep(0.01)
+    else:
+        # If the panel is off, sleep for a longer while
+        time.sleep(0.2)
 
 
 if __name__ == "__main__":
@@ -356,7 +359,6 @@ if __name__ == "__main__":
 
     while state.running:
         panel_update()
-        time.sleep(0.01)
     # When we get here, we are shutting down
     # Turn off the panel
     # pylint: disable=no-value-for-parameter
